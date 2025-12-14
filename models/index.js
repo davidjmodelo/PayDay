45982f50c29fbf154c5bfe03ded816b1f2336be2
Leonel Noriega-ROojas <leonelnr8@csu.fullerton.edu>
2025-12-14 13:31:50 -0800

    Delete models directory

--- models/index.js
+++
@@ -1,7 +0,0 @@
-import User from './User.js';
-import Post from './Post.js';
-import Bet from './Bet.js';
-import RobSuggestion from './RobSuggestion.js';
-import RobMessage from './RobMessage.js';
-
-export { User, Post, Bet, RobSuggestion, RobMessage };
